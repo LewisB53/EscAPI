07824b9de95e586f7005408bcbd60b95976fb282
Lewis <lewisburns630@googlemail.com>
2017-07-25 21:54:27 +0100

    add users and auth data

--- roomsApi/app/controllers/stats_controller.rb
+++ roomsApi/app/controllers/stats_controller.rb
@@ -1,7 +1,35 @@
 class StatsController < ApplicationController
   before_action :authenticate_user!
+  
   def index
-    stats = [ { name:'Lewis', level:7 },{name:'noob',level:1}]
-    render json: stats
+    stats = Stat.all  
+    render :json => stats.to_json()
   end
+
+  private
+  def stat_params
+    params.require(:stat).permit([:name, :level])
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+# class StatsController < ApplicationController
+#   before_action :authenticate_user!
+#   def index
+#     # render({json:{string:"hello"}})
+#     @stats = current_user.stats
+#     render( { json: @stats } )
+#   end
+# end
+
--- roomsApi/config/routes.rb
+++ roomsApi/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :rooms
   get 'stats' => 'stats#index'
+
+  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
+
+    resources :stats, defaults: {format: :json}
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
